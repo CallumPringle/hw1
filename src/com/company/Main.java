a6df7efded9b6d1124a2d941e62e4617d4e52838
Callum Pringle <CP211096@reigate.ac.uk>
2021-09-24 11:04:30 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+   Scanner sc =  new Scanner(System.in);
+   System.out.println("please enter a number");
+   int number = sc.nextInt();
+   String lastNum = "0";
+   for(int counter =1; counter <= number; counter++) {
+       lastNum = (lastNum + (counter));
+       System.out.println(lastNum);
+   }
+    for(int counter)
+    }
+}
